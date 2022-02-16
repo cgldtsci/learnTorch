decab8c0c81d1448c96d6be8f281b7197256716e
cgl <cgldtsci@163.com>
2022-02-16 23:55:52 +0800

    finish THGeneral.h

--- torch/lib/TH/THGeneral.c
+++ torch/lib/TH/THGeneral.c
@@ -1,6 +1,87 @@
 #include <stdio.h>
 
-void defaultTorchErrorHandlerFunction(const char *msg, void *data)
+#include "THGeneral.h"
+
+#ifndef TH_HAVE_THREAD
+#define __thread
+#endif
+
+#if (defined(__unix) || defined(_WIN32))
+  #if defined(__FreeBSD__)
+    #include <malloc_np.h>
+  #else
+    #include <malloc.h>
+  #endif
+#elif defined(__APPLE__)
+#include <malloc/malloc.h>
+#endif
+
+/* Torch Error Handling */
+static void defaultTorchErrorHandlerFunction(const char *msg, void *data)
 {
-    printf("defaultTorchErrorHandlerFunction\n");
+  printf("$ Error: %s\n", msg);
+  exit(-1);
+}
+
+static __thread void (*torchErrorHandlerFunction)(const char *msg, void *data) = defaultTorchErrorHandlerFunction;
+static __thread void *torchErrorHandlerData;
+
+void _THError(const char *file, const int line, const char *fmt, ...)
+{
+  char msg[2048];
+  va_list args;
+
+  /* vasprintf not standard */
+  /* vsnprintf: how to handle if does not exists? */
+  va_start(args, fmt);
+  int n = vsnprintf(msg, 2048, fmt, args);
+  va_end(args);
+
+  if(n < 2048) {
+    snprintf(msg + n, 2048 - n, " at %s:%d", file, line);
+  }
+
+  (*torchErrorHandlerFunction)(msg, torchErrorHandlerData);
+}
+
+void _THAssertionFailed(const char *file, const int line, const char *exp, const char *fmt, ...) {
+  char msg[1024];
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(msg, 1024, fmt, args);
+  va_end(args);
+  _THError(file, line, "Assertion `%s' failed. %s", exp, msg);
+}
+
+/* Torch Arg Checking Handling */
+static void defaultTorchArgErrorHandlerFunction(int argNumber, const char *msg, void *data)
+{
+  if(msg)
+    printf("$ Invalid argument %d: %s\n", argNumber, msg);
+  else
+    printf("$ Invalid argument %d\n", argNumber);
+  exit(-1);
+}
+
+static __thread void (*torchArgErrorHandlerFunction)(int argNumber, const char *msg, void *data) = defaultTorchArgErrorHandlerFunction;
+static __thread void *torchArgErrorHandlerData;
+
+void _THArgCheck(const char *file, int line, int condition, int argNumber, const char *fmt, ...)
+{
+  if(!condition) {
+    char msg[2048];
+    va_list args;
+
+    /* vasprintf not standard */
+    /* vsnprintf: how to handle if does not exists? */
+    va_start(args, fmt);
+    int n = vsnprintf(msg, 2048, fmt, args);
+    va_end(args);
+
+    if(n < 2048) {
+      snprintf(msg + n, 2048 - n, " at %s:%d", file, line);
+    }
+
+    (*torchArgErrorHandlerFunction)(argNumber, msg, torchArgErrorHandlerData);
+  }
 }
--- torch/lib/TH/test.c
+++ torch/lib/TH/test.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-
-void defaultTorchErrorHandlerFunction(const char *msg, void *data);
+#include "THGeneral.h"
 
 int main() {
     printf("Hello, World!\n");
-    defaultTorchErrorHandlerFunction(NULL, NULL);
+    M_PI;
+    USE_BLAS;
+    USE_LAPACK;
+    printf("start\n");
+// $ Error: wrong at /Users/cgl/Desktop/MyProject/learnTorch/torch/lib/TH/test.c:11
+//    THError("%s","wrong");
+// $ Invalid argument 5: message at /Users/cgl/Desktop/MyProject/learnTorch/torch/lib/TH/test.c:12
+//    THArgCheck(0,5,"%s","message");
+//$ Error: Assertion `5==3' failed.  at /Users/cgl/Desktop/MyProject/learnTorch/torch/lib/TH/test.c:14
+//    THAssert(5==3);
+    printf("end\n");
     return 0;
 }
